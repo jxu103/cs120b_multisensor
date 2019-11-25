fbb485282baaedf367f647f14539a655e86f14a4
Jingyuan Xu <jxu103@ucr.edu>
2019-11-24 22:32:19 -0800

    Updated temp Sensor header

--- multi_sensor/source/main.c
+++ multi_sensor/source/main.c
@@ -8,16 +8,20 @@
  *	code, is my own original work.
  */
 #include <avr/io.h>
+#include <dht.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #endif
 
+dht DHT;
+
 int main(void) {
     /* Insert DDR and PORT initializations */
+    DDRD 0xFF; PORTD 0x00;
 
     /* Insert your solution below */
     while (1) {
-
+        int check = DHT.read11(PORTD);
     }
     return 1;
 }
