bb0a5b58fefe9ab39978b3ba45f6be66daf1d420
Jingyuan Xu <jxu103@ucr.edu>
2019-11-25 21:21:21 -0800

    updated usart

--- multi_sensor/source/main.c
+++ multi_sensor/source/main.c
@@ -8,38 +8,20 @@
  *	code, is my own original work.
  */
 #include <avr/io.h>
+#include "usart.h"
 
-#define F_CPU 16000000// Clock Speed
-#define BAUD 9600
-#define MYUBRR (((F_CPU / (BAUD * 16UL))) - 1)
 
-void USART_Init( unsigned int ubrr);
-void USART_Transmit( unsigned char data );
-unsigned char USART_Receive( void );
+int main( void ){
+	DDRC = 0xFF, PORTC = 0x00;
 
-//char String[]="12345--Hello world--"; 
+	initUSART(0); // initializes USART0
+	unsigned char temp;
+	while (1) {
+		if ( USART_HasTransmitted(0) ) {
+			temp = USART_Receive(0);
+			PORTC = temp;
+	   }
 
-int main( void )
-{
-	USART_Init(MYUBRR);
-	while (1)
-	{
 	}
 	return 0;
-}
-
-void uart_init(void){	
-	UBRR0H = MYUBRR;
-	UBRR0L = MYUBRR;
-	
-	#if USE_2X
-		UCSR0A |= _BV(U2X0);
-	#else 
-		UCSR0A &= ~(_BV(U2X0));
-	#endif
-
-	//CONFIGURE: 8 BIT DATA, 1 STOP BIT, NO PARITY
-	
-	UCSR0C = _BV(UCSZ01) | _BV(UCSZ00); //SET 8 BIT DATA FRAME
-	UCSR0B = _BV(RXEN0) | _BV(TXEN0);   //ENABLE RX AND TX
 }
