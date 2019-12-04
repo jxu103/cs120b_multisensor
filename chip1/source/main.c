ca3a2635cc094ed22fc7320982ae38484ffde661
Jingyuan Xu <jxu103@wch136-02.cs.ucr.edu>
2019-12-04 09:57:44 -0800

    final

--- chip1/source/main.c
+++ chip1/source/main.c
@@ -17,6 +17,13 @@ int8_t humidity_int = 0;
 unsigned char temperature = 0x00;
 unsigned char rh = 0x00;
 
+unsigned short AD_convert = 0x0000;
+unsigned short MAX = 0xFF;
+
+void ADC_init(){
+    ADCSRA |= (1 << ADEN) | (1 << ADSC) | (1 << ADATE);
+}
+
 void output_temperature(unsigned char temperature) {
 	if(temperature >= 28) {
 		PORTC = 0xFF;
@@ -45,12 +52,14 @@ void output_temperature(unsigned char temperature) {
 }
 
 int main( void ){
+	DDRA = 0x00; PORTA = 0xFF;
 	DDRC = 0xFF; PORTC = 0x00;
 	DDRB = 0x00; PORTB = 0xFF;
 	DDRD = 0xFE; PORTD = 0x01;
 
 	initUSART(0); // initializes USART0
 	unsigned char warningFlag = 0x00;
+	ADC_init();
 	while (1) {
         warningFlag = 0x00;
 		//get temperature and humidity level
@@ -62,18 +71,35 @@ int main( void ){
 		else {
 			PORTC = 0xFF;
 		}
-		
+	
 		/*
 		set warningFlags
 		if bit 0 is set one, then low temperature (below 20C)
 		if bit 1 is set one, then high temperature (above 24C)
 		If bit 2 is set one, then low RH (below 20%)
 		if bit 3 is set one, then high RH (above 60%)
+		if bit 4 is set one, then lights are out;
 		*/
+		AD_convert = ADC;
+		if(AD_convert >= MAX/2) {
+			warningFlag |= 0x10;
+		}
 		if(temperature < 20) {
 			warningFlag |= 0x01;
 		}
-		_delay_ms(2000);
+		else if(temperature > 24) {
+			warningFlag |= 0x02;
+		}
+		if(rh < 20) {
+			warningFlag |= 0x04;
+		}
+		else if(rh > 60) {
+			warningFlag |= 0x08;
+		}
+		
+		
+		USART_Send(warningFlag,0);
+		_delay_ms(1500);
 		
 	}
 	return 0;
--- chip2/source/main.c
+++ chip2/source/main.c
@@ -11,14 +11,38 @@
 #include <util/delay.h>
 #include "usart_ATmega1284.h"
 
+void set_PWM(double frequency) {
+	static double current_frequency;
+	if (frequency != current_frequency) {
 
+		if (!frequency) TCCR3B &= 0x08; 
+		else TCCR3B |= 0x03; 
+		if (frequency < 0.954) OCR3A = 0xFFFF;
+		else if (frequency > 31250) OCR3A = 0x0000;
+		else OCR3A = (short)(8000000 / (128 * frequency)) - 1;
+
+		TCNT3 = 0; 
+		current_frequency = frequency;
+	}
+}
+void PWM_on() {
+	TCCR3A = (1 << COM3A0);
+	TCCR3B = (1 << WGM32) | (1 << CS31) | (1 << CS30);
+	set_PWM(0);
+}
+void PWM_off() {
+	TCCR3A = 0x00;
+	TCCR3B = 0x00;
+}
 
 int main( void ){
 	DDRD = 0xFE; PORTD = 0x01;
 	DDRA = 0xFF; PORTA = 0x00;
+	DDRB = 0xFF; PORTB = 0x00;
 
 	initUSART(0); // initializes USART0
 	unsigned char warningFlag = 0x00;
+	PWM_on();
 	while (1) {
 		//receive warning info
 		if(USART_HasReceived(0)) {
@@ -30,8 +54,16 @@ int main( void ){
 		if bit 1 is set one, then high temperature (above 24C)
 		If bit 2 is set one, then low RH (below 20%)
 		if bit 3 is set one, then high RH (above 60%)
+		if bit 4 is set one, then lights are out
 		*/
-		PORTC = warningFlag;
+		PORTA = warningFlag;
+		if((warningFlag & 0x10) == 0x10){
+		set_PWM(261.63);
+		}
+		else{
+		set_PWM(0);
+		}
+		
 		
 	}
 	return 0;
