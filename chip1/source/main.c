274ea0d77b79daf0329208e474b70d2e8ad6c8f6
Jingyuan Xu <jxu103@wch132-04.cs.ucr.edu>
2019-12-03 20:49:40 -0800

    added PWM & adc

--- chip1/source/main.c
+++ chip1/source/main.c
@@ -17,40 +17,100 @@ uint16_t temperature_int = 0;
 uint16_t humidity_int = 0;
 unsigned char temperature = 0x00;
 unsigned char rh = 0x00;
-
-/*
-if bit 0 is set one, then low temperature (below 20C)
-if bit 1 is set one, then high temperature (above 24C)
-If bit 2 is set one, then low RH (below 20%)
-if bit 3 is set one, then high RH (above 60%)
-*/
 unsigned char warningFlag = 0x00;
 
-void print_LCD(unsigned char temperature, unsigned char rh) {
-	unsigned char* LCD_template = "Current:           Temp:  . C    Humidity:    %";
+unsigned short AD_convert = 0x0000;
+unsigned short MAX = 0x0000;
+
+void ADC_init(){
+    ADCSRA |= (1 << ADEN) | (1 << ADSC) | (1 << ADATE);
+}
+
+void print_LCD(uint16_t temperature, uint16_t rh) {
+	unsigned char* LCD_template = "Current:        Temp:  . C    Humidity:    %";
 	LCD_DisplayString(1, LCD_template);
 	
+	//print temperature
+	//print 10s
+	unsigned char count = 0;
+	while(temperature >= 100) {
+		count ++;
+		temperature -= 100;
+	}
+	LCD_Cursor(20);
+	LCD_WriteData(count + '0');
+	
+	//print 1s
+	count = 0;
+	while(temperature >= 10) {
+		count ++;
+		temperature -= 10;
+	}
+	LCD_Cursor(21);
+	LCD_WriteData(count + '0');
+	
+	//print decimals
+	LCD_Cursor(24);
+	LCD_WriteData((unsigned char)temperature + '0');
+	
+	
+	//print temperature
+	//print 100s
+	count = 0;
+	while(rh >= 100) {
+		count ++;
+		rh -= 100;
+	}
+	LCD_Cursor(20);
+	LCD_WriteData(count + '0');
+	
+	//print 10s
+	count = 0;
+	while(rh >= 10) {
+		count ++;
+		rh -= 10;
+	}
+	LCD_Cursor(21);
+	LCD_WriteData(count + '0');
+	
+	//print 1s
+	LCD_Cursor(22);
+	LCD_WriteData((unsigned char)rh + '0');
 }
 
 int main( void ){
+	DDRA = 0x00; PORTA = 0xFF;
+	DDRB = 0x00; PORTB = 0xFF;
 	DDRC = 0xFF; PORTC = 0x00;
 	DDRD = 0xFE; PORTD = 0x01;
 
 	initUSART(0); // initializes USART0
 	LCD_init();
+	ADC_init();
 	
 	unsigned char temp;
 	while (1) {
+        AD_convert = ADC;
+        if(AD_convert <= 0x0C7;
         
 		//get temperature and humidity level
         if (dht_GetTempUtil(&temperature_int, &humidity_int) != -1) {
 			temperature = temperature_int / 10;
 			rh = humidity_int / 10;
+			print_LCD(temperature_int, humidity_int);
 		}
-		
-		print_LCD(temperature,rh);
+		else {
+			LCD_ClearScreen();
+			LCD_DisplayString(1,"Sensor Error");
+		}
 
 		//check levels, output level and send out warnings if needed
+		/*
+		if bit 0 is set one, then low temperature (below 20C)
+		if bit 1 is set one, then high temperature (above 24C)
+		If bit 2 is set one, then low RH (below 20%)
+		if bit 3 is set one, then high RH (above 60%)
+		*/
 		warningFlag = 0x00;
 		if(temperature < 20 ) {
 			warningFlag = warningFlag || 0x01;
--- chip2/source/main.c
+++ chip2/source/main.c
@@ -12,9 +12,34 @@
 #include "usart_ATmega1284.h"
 
 
+void set_PWM(double frequency) {
+	static double current_frequency;
+	if (frequency != current_frequency) {
+
+		if (!frequency) TCCR3B &= 0x08; 
+		else TCCR3B |= 0x03; 
+		if (frequency < 0.954) OCR3A = 0xFFFF;
+		else if (frequency > 31250) OCR3A = 0x0000;
+		else OCR3A = (short)(8000000 / (128 * frequency)) - 1;
+
+		TCNT3 = 0; 
+		current_frequency = frequency;
+	}
+}
+void PWM_on() {
+	TCCR3A = (1 << COM3A0);
+	TCCR3B = (1 << WGM32) | (1 << CS31) | (1 << CS30);
+	set_PWM(0);
+}
+void PWM_off() {
+	TCCR3A = 0x00;
+	TCCR3B = 0x00;
+}
+
+
 int main( void ){
 	DDRD = 0xFE, PORTD = 0x01;
-	DDRC = 0xFF, PORTC = 0x00;
+	DDRA = 0xFF, PORTA = 0x00;
 
 	initUSART(0); // initializes USART0
 	unsigned char warningFlag = 0x00;
@@ -34,6 +59,13 @@ int main( void ){
 		*/
 		PORTC = warningFlag;
 		
+		if(warningFlag & 0x01 == 0x01) {
+			PWM_on();
+			set_PWM(261.63);
+		}
+		else {
+			PWM_off;
+		}
 		
 	}
 	return 0;
